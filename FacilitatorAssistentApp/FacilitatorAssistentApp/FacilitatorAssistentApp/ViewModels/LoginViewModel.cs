4f935b8e2ec1684aa50f3206c63b37d0f9394c6d
Blackfoxx16 <castillo.dev93@hotmail.com>
2022-10-07 01:46:09 -0500

    Se modifica la vista loginpage, se realiza binding con la nueva clase LoginViewModel, y se agrega la BaseViewModel.  Pendiente de modificar el ActivityIndicator por nugget Acr.UserDialogs by AllanRitchie

---
+++ FacilitatorAssistentApp/FacilitatorAssistentApp/FacilitatorAssistentApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Forms;
+
+namespace FacilitatorAssistentApp.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+
+        #region Attributes
+
+        private string user;
+        private string password;
+
+        #endregion
+
+        #region Properties
+
+        public string userText {
+            get { return this.user;  }
+            set { SetValue(ref this.user, value); }
+        }
+
+        public string pwdText
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        #endregion
+
+        #region Commands
+
+
+        public ICommand LoginCommad
+        {
+            get { return new RelayCommand(Login); }
+            set { }
+        }
+
+        public ICommand goRegisterPageCommand => new Command(() => { Application.Current.MainPage.Navigation.PopAsync(); });
+
+        #endregion
+
+        #region Methods
+
+        private void Login()
+        {
+            
+            if (validateStringField(userText) == "guadalupe" && validateStringField(pwdText) == "4503")
+            {
+                Application.Current.MainPage.DisplayAlert("Correct","Bindig succesfuly, you are loged.", "Ok");
+            }
+            else {
+                Application.Current.MainPage.DisplayAlert("Fail","Binding Correct, but you can't login", "Ok");
+            }
+        }
+
+
+        private string validateStringField(string data) {
+            return string.IsNullOrEmpty(data) ? "" : data.ToString();
+        }
+        
+        #endregion
+
+        #region Constructors
+
+        #endregion
+    }
+}
+
--- FacilitatorAssistentApp/FacilitatorAssistentApp/FacilitatorAssistentApp/Views/AccessViews/LoginPage.xaml.cs
+++ FacilitatorAssistentApp/FacilitatorAssistentApp/FacilitatorAssistentApp/Views/AccessViews/LoginPage.xaml.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Windows.Input;
+using FacilitatorAssistentApp.ViewModels;
 using Xamarin.Forms;
 
 namespace FacilitatorAssistentApp.Views.AccessViews
 {
     public partial class LoginPage : ContentPage
     {
+
         public LoginPage()
         {
-
             InitializeComponent();
+            BindingContext = new LoginViewModel();
         }
 
-        private void ConfirmEvent_Clicked(System.Object sender, System.EventArgs e)
-        {
-
-        }
     }
 }
 
